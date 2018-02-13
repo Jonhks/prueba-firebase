ca1edc580b201952634a06a2c9426e51f4aa7dfd
Jonh <jonathan@laboratoria.la>
2018-02-13 17:38:54 -0600

    probando firebase 2

--- js/app.js
+++ js/app.js
@@ -44,3 +44,41 @@
   //     // ...
   //   });
   // }
+
+  var botonGoogle = document.getElementById("buttonGoogle");
+  botonGoogle.addEventListener("click", accesoCongoogle);
+  var botonGoogle = document.getElementById("buttonFace");
+  botonGoogle.addEventListener("click", accesoFacebook);
+
+
+
+  function accesoCongoogle () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    autenticacion(provider);
+  }
+
+  function accesoFacebook (){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    autenticacion(provider);
+    
+  }
+
+ function autenticacion ( provider){
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+ }
---
+++ js/credenciales.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCu6CPAOqsNkfkhxdvZ09Q7GWElVdrGN10",
+    authDomain: "probando-firebase-29280.firebaseapp.com",
+    databaseURL: "https://probando-firebase-29280.firebaseio.com",
+    projectId: "probando-firebase-29280",
+    storageBucket: "probando-firebase-29280.appspot.com",
+    messagingSenderId: "35153996953"
+  };
+  firebase.initializeApp(config);
